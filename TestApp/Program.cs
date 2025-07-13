1186453a37580c6e57f9f43d9de06feaed1328d8
Jian Ting <me@jtpox.com>
2025-07-13 14:32:40 +0800

    ADD: Test console app.

---
+++ TestApp/Program.cs
@@ -0,0 +1,37 @@
+﻿using YaraXSharp;
+namespace TestApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var yara = new YaraX(YRX_COMPILE_FLAGS.YRX_ERROR_ON_SLOW_PATTERN, YRX_COMPILE_FLAGS.YRX_DISABLE_INCLUDES);
+                yara.AddRuleFile(Path.Combine(Environment.CurrentDirectory, "../../../", "eicar.yar"));
+                yara.AddRuleFile(Path.Combine(Environment.CurrentDirectory, "../../../", "eitwo.yar"));
+                var rules = yara.Build();
+
+                Console.WriteLine($"Number of rules: {yara.RulesCount()}");
+
+                Scanner scanner = new Scanner(rules);
+                scanner.Scan(Path.Combine(Environment.CurrentDirectory, "eicar.txt"));
+                scanner.Scan(Path.Combine(Environment.CurrentDirectory, "eicar.txt"));
+                List<Rule> results = scanner.Results();
+                Console.WriteLine($"Matches: {results.Count}");
+
+                foreach (Rule rule in results)
+                {
+                    Console.WriteLine(rule.Metadata["malware_family"]);
+                }
+
+                // Make sure to destroy.
+                scanner.Destroy();
+                yara.Destroy();
+            } catch (YrxException ex)
+            {
+                Console.Write(ex.Message);
+            }
+        }
+    }
+}
---
+++ TestApp/eitwo.yar
@@ -0,0 +1,22 @@
+﻿rule malw_eicar2  {
+	
+	meta:
+
+		description = "Rule to detect the EICAR pattern"
+      	author = "Marc Rivero | McAfee ATR Team"
+      	reference = "https://www.eicar.org/"
+      	rule_version = "v1"
+        malware_type = "eicar2"
+        malware_family = "W32/Eicar2"
+        actor_type = "Unknown"
+        actor_group = "Unknown"
+		hash = "275a021bbfb6489e54d471899f7db9d1663fc695ec2fe2a2c4538aabf651fd0f"
+
+	strings:
+
+		$s1 = "X5O!P%@AP[4\\PZX54(P^)7CC)7}$EICAR-STANDARD-ANTIVIRUS-TEST-FILE!$H+H*" fullword ascii
+
+	condition:
+
+	 	any of them
+}
