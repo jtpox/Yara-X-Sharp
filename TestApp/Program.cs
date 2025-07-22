af439c389fb073484123e7ce5e3fed15f34c5f49
Jian Ting <me@jtpox.com>
2025-07-22 15:36:37 +0800

    UPDATE: TestApp to use disposable classes.

--- TestApp/Program.cs
+++ TestApp/Program.cs
@@ -7,7 +7,7 @@ namespace TestApp
         {
             try
             {
-                var yara = new YaraX(YRX_COMPILE_FLAGS.YRX_ERROR_ON_SLOW_PATTERN, YRX_COMPILE_FLAGS.YRX_DISABLE_INCLUDES);
+                /* var yara = new YaraX(YRX_COMPILE_FLAGS.YRX_ERROR_ON_SLOW_PATTERN, YRX_COMPILE_FLAGS.YRX_DISABLE_INCLUDES);
                 yara.AddRuleFile(Path.Combine(Environment.CurrentDirectory, "../../../", "eicar.yar"));
                 yara.AddRuleFile(Path.Combine(Environment.CurrentDirectory, "../../../", "eitwo.yar"));
                 var rules = yara.Build();
@@ -23,11 +23,33 @@ namespace TestApp
                 {
                     Console.WriteLine($"Pattern match count: {rule.Patterns.Count}");
                     Console.WriteLine(rule.Metadata["malware_family"]);
-                }
+                } */
 
                 // Make sure to destroy.
-                scanner.Destroy();
-                yara.Destroy();
+                /* scanner.Destroy();
+                yara.Destroy(); */
+
+                using (var yara = new YaraX(YRX_COMPILE_FLAGS.YRX_ERROR_ON_SLOW_PATTERN, YRX_COMPILE_FLAGS.YRX_DISABLE_INCLUDES))
+                {
+                    yara.AddRuleFile(Path.Combine(Environment.CurrentDirectory, "../../../", "eicar.yar"));
+                    yara.AddRuleFile(Path.Combine(Environment.CurrentDirectory, "../../../", "eitwo.yar"));
+                    var rules = yara.Build();
+
+                    Console.WriteLine($"Number of rules: {yara.RulesCount()}");
+
+                    using (Scanner scanner = new Scanner(rules, YRX_SCANNER_FLAGS.LOAD_METADATA, YRX_SCANNER_FLAGS.LOAD_PATTERNS))
+                    {
+                        scanner.Scan(Path.Combine(Environment.CurrentDirectory, "eicar.txt"));
+                        List<Rule> results = scanner.Results();
+                        Console.WriteLine($"Matches: {results.Count}");
+
+                        foreach (Rule rule in results)
+                        {
+                            Console.WriteLine($"Pattern match count: {rule.Patterns.Count}");
+                            Console.WriteLine(rule.Metadata["malware_family"]);
+                        }
+                    }
+                }
             } catch (YrxException ex)
             {
                 Console.Write(ex.Message);
