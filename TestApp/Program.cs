40191e78b25329290756b849e0d6ccdef1e59efd
Jian Ting <me@jtpox.com>
2025-07-23 00:55:42 +0800

    UPDATE: Remove commented code on TestApp.

--- TestApp/Program.cs
+++ TestApp/Program.cs
@@ -7,31 +7,6 @@ namespace TestApp
         {
             try
             {
-                /* var yara = new YaraX(YRX_COMPILE_FLAGS.YRX_ERROR_ON_SLOW_PATTERN, YRX_COMPILE_FLAGS.YRX_DISABLE_INCLUDES);
-                yara.AddRuleFile(Path.Combine(Environment.CurrentDirectory, "../../../", "eicar.yar"));
-                yara.AddRuleFile(Path.Combine(Environment.CurrentDirectory, "../../../", "eitwo.yar"));
-
-                var (rules, errors, warnings) = yara.Build();
-
-                if (errors.Length != 0) _LoopErrorFormat(errors);
-                if (warnings.Length != 0) _LoopErrorFormat(warnings);
-
-                Console.WriteLine($"Number of rules: {yara.RulesCount()}");
-
-                Scanner scanner = new Scanner(rules, YRX_SCANNER_FLAGS.LOAD_METADATA, YRX_SCANNER_FLAGS.LOAD_PATTERNS);
-                scanner.Scan(Path.Combine(Environment.CurrentDirectory, "eicar.txt"));
-                List<Rule> results = scanner.Results();
-                Console.WriteLine($"Matches: {results.Count}");
-
-                foreach (Rule rule in results)
-                {
-                    Console.WriteLine($"Pattern match count: {rule.Patterns.Count}");
-                    Console.WriteLine(rule.Metadata["malware_family"]);
-                }
-
-                // Make sure to destroy.
-                scanner.Destroy();
-                yara.Destroy(); */
                 using (var yara = new YaraX(YRX_COMPILE_FLAGS.YRX_ERROR_ON_SLOW_PATTERN, YRX_COMPILE_FLAGS.YRX_DISABLE_INCLUDES))
                 {
                     yara.AddRuleFile(Path.Combine(Environment.CurrentDirectory, "../../../", "eicar.yar"));
