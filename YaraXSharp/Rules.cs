834ea7ab47ce905dfa31fc652c053679157443c9
Jian Ting <me@jtpox.com>
2025-07-31 00:42:39 +0800

    ADD: Rules (plural) to handle Yara X rules functions.

---
+++ YaraXSharp/Rules.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YaraXSharp
+{
+    public class Rules : IDisposable
+    {
+        internal IntPtr _pointer;
+        public Rules(IntPtr rulesPtr)
+        {
+            _pointer = rulesPtr;
+        }
+
+        public int Count()
+        {
+            return YaraX.yrx_rules_count(_pointer);
+        }
+
+        public void Destroy()
+        {
+            if (_pointer != IntPtr.Zero) YaraX.yrx_rules_destroy(_pointer);
+            _pointer = IntPtr.Zero;
+        }
+
+        public void Dispose()
+        {
+            Destroy();
+        }
+    }
+}
