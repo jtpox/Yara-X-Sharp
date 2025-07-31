6e9d6c3fc6c532ee5926461c95ca3258cc6e3385
Jian Ting <me@jtpox.com>
2025-07-31 11:43:29 +0800

    UPDAT:E TestApp to include import and export of serialized rules.

--- TestApp/Program.cs
+++ TestApp/Program.cs
@@ -8,6 +8,7 @@ namespace TestApp
             try
             {
 
+                Console.WriteLine("Test 1");
                 using (Compiler yara = new Compiler(YRX_COMPILE_FLAGS.YRX_ERROR_ON_SLOW_PATTERN, YRX_COMPILE_FLAGS.YRX_DISABLE_INCLUDES))
                 {
                     yara.AddRuleFile(Path.Combine(Environment.CurrentDirectory, "../../../", "eicar.yar"));
@@ -17,6 +18,50 @@ namespace TestApp
                     if (errors.Length != 0) _LoopErrorFormat(errors);
                     if (warnings.Length != 0) _LoopErrorFormat(warnings);
 
+                    Console.WriteLine($"Number of rules: {rules.Count()}");
+
+                    using (Scanner scanner = new Scanner(rules, YRX_SCANNER_FLAGS.LOAD_METADATA, YRX_SCANNER_FLAGS.LOAD_PATTERNS))
+                    {
+                        scanner.Scan(Path.Combine(Environment.CurrentDirectory, "eicar.txt"));
+                        List<Match> results = scanner.Results();
+                        Console.WriteLine($"Matches: {results.Count}");
+
+                        foreach (Match rule in results)
+                        {
+                            Console.WriteLine($"Pattern match count: {rule.Patterns.Count}");
+                            Console.WriteLine(rule.Metadata["malware_family"]);
+                        }
+                    }
+                }
+
+                Console.WriteLine("---");
+
+                Console.WriteLine("Test 2");
+                if (File.Exists("./ydb.dat")) File.Delete("./ydb.dat");
+                using (Compiler yara = new Compiler(YRX_COMPILE_FLAGS.YRX_ERROR_ON_SLOW_PATTERN, YRX_COMPILE_FLAGS.YRX_DISABLE_INCLUDES))
+                {
+                    yara.AddRuleFile(Path.Combine(Environment.CurrentDirectory, "../../../", "eicar.yar"));
+                    yara.AddRuleFile(Path.Combine(Environment.CurrentDirectory, "../../../", "eitwo.yar"));
+                    var (rules, errors, warnings) = yara.Build();
+
+                    if (errors.Length != 0) _LoopErrorFormat(errors);
+                    if (warnings.Length != 0) _LoopErrorFormat(warnings);
+
+                    var export = rules.Export();
+                    using (var fs = new FileStream("./ydb.dat", FileMode.Create, FileAccess.Write))
+                    {
+                        fs.Write(export, 0, export.Length);
+                        Console.WriteLine("Serialized rules exported to ydb.dat");
+                    }
+                }
+
+                Console.WriteLine("---");
+
+                Console.WriteLine("Test 3");
+                using (Rules rules = new Rules())
+                {
+                    var serializedRules = File.ReadAllBytes("./ydb.dat");
+                    rules.Import(serializedRules);
                     Console.WriteLine($"Number of rules: {rules.Count()}");
 
                     using (Scanner scanner = new Scanner(rules, YRX_SCANNER_FLAGS.LOAD_METADATA, YRX_SCANNER_FLAGS.LOAD_PATTERNS))
