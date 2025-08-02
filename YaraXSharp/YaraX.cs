12548b23f6957368c933e964ec765d4b960a27f3
Jian Ting <me@jtpox.com>
2025-08-02 12:21:30 +0800

    UPDATE: Remove dll extension in DLLImport.

--- YaraXSharp/YaraX.cs
+++ YaraXSharp/YaraX.cs
@@ -77,61 +77,61 @@ namespace YaraXSharp
     }
     public static class YaraX
     {
-        [DllImport("yara_x_capi.dll")]
+        [DllImport("yara_x_capi")]
         public static extern void yrx_buffer_destroy(IntPtr buffer);
 
         /*
          * Compiler Functions
          */
-        [DllImport("yara_x_capi.dll")]
+        [DllImport("yara_x_capi")]
         public static extern YRX_RESULT yrx_compiler_create(int flags, out IntPtr compiler);
 
-        [DllImport("yara_x_capi.dll")]
+        [DllImport("yara_x_capi")]
         public static extern void yrx_compiler_destroy(IntPtr compiler);
 
-        [DllImport("yara_x_capi.dll")]
+        [DllImport("yara_x_capi")]
         public static extern IntPtr yrx_compiler_build(IntPtr compiler);
 
-        [DllImport("yara_x_capi.dll")]
+        [DllImport("yara_x_capi")]
         public static extern YRX_RESULT yrx_compiler_add_source(IntPtr compiler, string src);
 
-        [DllImport("yara_x_capi.dll")]
+        [DllImport("yara_x_capi")]
         public static extern YRX_RESULT yrx_compiler_add_source_with_origin(IntPtr compiler, string src, string origin);
 
-        [DllImport("yara_x_capi.dll")]
+        [DllImport("yara_x_capi")]
         public static extern YRX_RESULT yrx_compiler_errors_json(IntPtr compiler, out IntPtr buf);
 
-        [DllImport("yara_x_capi.dll")]
+        [DllImport("yara_x_capi")]
         public static extern YRX_RESULT yrx_compiler_warnings_json(IntPtr compiler, out IntPtr buf);
 
-        [DllImport("yara_x_capi.dll")]
+        [DllImport("yara_x_capi")]
         public static extern YRX_RESULT yrx_compiler_ignore_module(IntPtr compiler, string module);
 
-        [DllImport("yara_x_capi.dll")]
+        [DllImport("yara_x_capi")]
         public static extern YRX_RESULT yrx_compiler_ban_module(IntPtr compiler, string module, string error_title, string error_msg);
 
-        [DllImport("yara_x_capi.dll")]
+        [DllImport("yara_x_capi")]
         public static extern YRX_RESULT yrx_compiler_new_namespace(IntPtr compiler, string name);
 
-        [DllImport("yara_x_capi.dll")]
+        [DllImport("yara_x_capi")]
         public static extern YRX_RESULT yrx_compiler_define_global_str(IntPtr compiler, string identifier, string value);
 
-        [DllImport("yara_x_capi.dll")]
+        [DllImport("yara_x_capi")]
         public static extern YRX_RESULT yrx_compiler_define_global_bool(IntPtr compiler, string identifier, bool value);
-        [DllImport("yara_x_capi.dll")]
+        [DllImport("yara_x_capi")]
         public static extern YRX_RESULT yrx_compiler_define_global_int(IntPtr compiler, string identifier, int value);
 
-        [DllImport("yara_x_capi.dll")]
+        [DllImport("yara_x_capi")]
         public static extern YRX_RESULT yrx_compiler_define_global_float(IntPtr compiler, string identifier, double value);
 
 
         /*
          * Rule Functions
          */
-        [DllImport("yara_x_capi.dll")]
+        [DllImport("yara_x_capi")]
         public static extern void yrx_rules_destroy(IntPtr rules);
 
-        [DllImport("yara_x_capi.dll")]
+        [DllImport("yara_x_capi")]
         public static extern int yrx_rules_count(IntPtr rules);
 
         public delegate void YRX_RULE_CALLBACK(IntPtr rule);
@@ -140,63 +140,63 @@ namespace YaraXSharp
         public delegate void YRX_TAGS_CALLBACK(IntPtr tag);
         public delegate void YRX_PATTERN_CALLBACK(IntPtr pattern);
 
-        [DllImport("yara_x_capi.dll")]
+        [DllImport("yara_x_capi")]
         public static extern YRX_RESULT yrx_rules_iter(IntPtr rules, YRX_RULE_CALLBACK callback);
 
-        [DllImport("yara_x_capi.dll")]
+        [DllImport("yara_x_capi")]
         public static extern YRX_RESULT yrx_rules_iter_imports(IntPtr rules, YRX_IMPORTS_CALLBACK callback);
 
-        [DllImport("yara_x_capi.dll")]
+        [DllImport("yara_x_capi")]
         public static extern IntPtr yrx_rule_iter_metadata(IntPtr rule, YRX_METADATA_CALLBACK callback);
 
-        [DllImport("yara_x_capi.dll")]
+        [DllImport("yara_x_capi")]
         public static extern IntPtr yrx_rule_iter_tags(IntPtr rule, YRX_TAGS_CALLBACK callback);
 
-        [DllImport("yara_x_capi.dll")]
+        [DllImport("yara_x_capi")]
         public static extern IntPtr yrx_rule_iter_patterns(IntPtr rule, YRX_PATTERN_CALLBACK callback);
 
-        [DllImport("yara_x_capi.dll")]
+        [DllImport("yara_x_capi")]
         public static extern YRX_RESULT yrx_pattern_identifier(IntPtr pattern, out IntPtr identifier, out int length);
 
-        [DllImport("yara_x_capi.dll")]
+        [DllImport("yara_x_capi")]
         public static extern YRX_RESULT yrx_rule_identifier(IntPtr rule, out IntPtr identifier, out int length);
 
-        [DllImport("yara_x_capi.dll")]
+        [DllImport("yara_x_capi")]
         public static extern YRX_RESULT yrx_rule_namespace(IntPtr rule, out IntPtr identifier, out int length);
 
-        [DllImport("yara_x_capi.dll")]
+        [DllImport("yara_x_capi")]
         public static extern YRX_RESULT yrx_rules_serialize(IntPtr rules, out IntPtr buf);
 
-        [DllImport("yara_x_capi.dll")]
+        [DllImport("yara_x_capi")]
         public static extern YRX_RESULT yrx_rules_deserialize(byte[] data, long len, out IntPtr rules);
 
         /*
          * Scanner Functions
          */
-        [DllImport("yara_x_capi.dll")]
+        [DllImport("yara_x_capi")]
         public static extern YRX_RESULT yrx_scanner_set_timeout(IntPtr scanner, long timeout);
 
-        [DllImport("yara_x_capi.dll")]
+        [DllImport("yara_x_capi")]
         public static extern YRX_RESULT yrx_scanner_create(IntPtr rules, out IntPtr scanner);
 
-        [DllImport("yara_x_capi.dll")]
+        [DllImport("yara_x_capi")]
         public static extern void yrx_scanner_destroy(IntPtr scanner);
 
-        [DllImport("yara_x_capi.dll")]
+        [DllImport("yara_x_capi")]
         public static extern YRX_RESULT yrx_scanner_scan(IntPtr scanner, byte[] data, long len);
 
-        [DllImport("yara_x_capi.dll")]
+        [DllImport("yara_x_capi")]
         public static extern YRX_RESULT yrx_scanner_on_matching_rule(IntPtr scanner, YRX_RULE_CALLBACK callback);
 
-        [DllImport("yara_x_capi.dll")]
+        [DllImport("yara_x_capi")]
         public static extern YRX_RESULT yrx_scanner_set_global_str(IntPtr compiler, string identifier, string value);
 
-        [DllImport("yara_x_capi.dll")]
+        [DllImport("yara_x_capi")]
         public static extern YRX_RESULT yrx_scanner_set_global_bool(IntPtr compiler, string identifier, bool value);
-        [DllImport("yara_x_capi.dll")]
+        [DllImport("yara_x_capi")]
         public static extern YRX_RESULT yrx_scanner_set_global_int(IntPtr compiler, string identifier, int value);
 
-        [DllImport("yara_x_capi.dll")]
+        [DllImport("yara_x_capi")]
         public static extern YRX_RESULT yrx_scanner_set_global_float(IntPtr compiler, string identifier, double value);
     }
 }
